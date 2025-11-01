1f49306d5a85a7dc7da6d9a8a329768d3bc15043
bailwillharr <baileyharrison04@outlook.com>
2025-11-01 20:46:17 +0000

    Add CRT shader

--- mario/mario_scene.gd
+++ mario/mario_scene.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pass;
 	# initialisation
-	get_window().size = Vector2i(640, 480)
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	#get_window().size = Vector2i(640, 480)
+	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	pass;
