222cd30305eb6465fafb1e399997756116254104
Nader Takruri <n.takrouri@hotmail.com>
2025-11-01 20:42:59 +0000

    More dino

--- gardino/dino.gd
+++ gardino/dino.gd
@@ -1,29 +1,32 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 200
 var screen_size
 
 
 func _ready():
+	$AnimatedSprite2D.play("idle")
 	screen_size = get_viewport_rect().size
 
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("right"):
+		$AnimatedSprite2D.flip_h = false
 		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("left"):
+		$AnimatedSprite2D.flip_h = true
 		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("down"):
 		velocity.y += 0.5
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("up"):
 		velocity.y -= 0.5
 
-	if velocity.length() > 0:
+	if velocity != Vector2.ZERO:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		$AnimatedSprite2D.play("walk")
 	else:
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.play("idle")
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
