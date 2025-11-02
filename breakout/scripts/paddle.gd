4e4e70d845b6579efd556391601bd4a3941abe7e
bailwillharr <baileyharrison04@outlook.com>
2025-11-02 04:11:56 +0000

    Fix breakout screen cutoff

--- breakout/scripts/paddle.gd
+++ breakout/scripts/paddle.gd
@@ -5,7 +5,7 @@ const SPEED = 1000.0 # paddle speed when moving
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
--- frogger/scripts/Player.gd
+++ frogger/scripts/Player.gd
@@ -7,27 +7,27 @@ var moving := false
 var target_position: Vector2
 
 func _ready():
-    target_position = position
-    add_to_group("player")  # let logs find you
+	target_position = position
+	add_to_group("player")  # let logs find you
 
 func _physics_process(delta):
-    if moving:
-        position = position.move_toward(target_position, hop_speed)
-        if position == target_position:
-            moving = false
+	if moving:
+		position = position.move_toward(target_position, hop_speed)
+		if position == target_position:
+			moving = false
 
 func _input(event):
-    if moving:
-        return
-    if event.is_action_pressed("ui_up"):
-        hop(Vector2.UP)
-    elif event.is_action_pressed("ui_down"):
-        hop(Vector2.DOWN)
-    elif event.is_action_pressed("ui_left"):
-        hop(Vector2.LEFT)
-    elif event.is_action_pressed("ui_right"):
-        hop(Vector2.RIGHT)
+	if moving:
+		return
+	if event.is_action_pressed("ui_up"):
+		hop(Vector2.UP)
+	elif event.is_action_pressed("ui_down"):
+		hop(Vector2.DOWN)
+	elif event.is_action_pressed("ui_left"):
+		hop(Vector2.LEFT)
+	elif event.is_action_pressed("ui_right"):
+		hop(Vector2.RIGHT)
 
 func hop(dir: Vector2):
-    target_position = position + dir * hop_distance
-    moving = true
+	target_position = position + dir * hop_distance
+	moving = true
