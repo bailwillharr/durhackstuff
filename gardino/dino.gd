e7240ab9824b827c10ee28b83271bb7758aedb0f
Nader Takruri <n.takrouri@hotmail.com>
2025-11-02 01:38:01 +0000

    anim

--- gardino/dino.gd
+++ gardino/dino.gd
@@ -1,31 +1,42 @@
 extends CharacterBody2D
 
-@export var speed = 200
-var screen_size
-
+var tile_size = 64
+var inputs = {"right": Vector2.RIGHT,
+			"left": Vector2.LEFT,
+			"up": Vector2.UP,
+			"down": Vector2.DOWN}
+@onready var ray = $ray
+var animation_speed = 5
+var moving = false
 
 func _ready():
-	$AnimatedSprite2D.play("idle")
-	screen_size = get_viewport_rect().size
+	$visuals.play("idle")
+	position = position.snapped(Vector2.ONE * tile_size)
+	# position += Vector2.ONE * tile_size / 2
 
+func _unhandled_input(event):
+	if moving:
+		return
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			if Input.is_action_pressed("right"):
+				$visuals.flip_h = false
+				velocity.x += 1
+			if Input.is_action_pressed("left"):
+				$visuals.flip_h = true
+			$visuals.play("walk")
+			move(dir)
+			return
+	$visuals.play("idle")
 
-func _process(delta):
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed("right"):
-		$AnimatedSprite2D.flip_h = false
-		velocity.x += 1
-	if Input.is_action_pressed("left"):
-		$AnimatedSprite2D.flip_h = true
-		velocity.x -= 1
-	if Input.is_action_pressed("down"):
-		velocity.y += 0.5
-	if Input.is_action_pressed("up"):
-		velocity.y -= 0.5
-
-	if velocity != Vector2.ZERO:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play("walk")
-	else:
-		$AnimatedSprite2D.play("idle")
-
-	position += velocity * delta
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		var tween = create_tween()
+		tween.tween_property(
+			self, "position", position + inputs[dir] * tile_size, 1.0 / animation_speed
+		).set_trans(Tween.TRANS_LINEAR)
+		moving = true
+		await tween.finished
+		moving = false
